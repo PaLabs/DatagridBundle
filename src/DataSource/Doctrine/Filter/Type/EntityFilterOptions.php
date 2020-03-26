d05d54ed44524374009922f85f332a6081258fc9
lewbor <boris.alperin@gmail.com>
2020-03-26 13:12:31 +0700

    Fixed EntityFilterOptions

--- src/DataSource/Doctrine/Filter/Type/EntityFilterOptions.php
+++ src/DataSource/Doctrine/Filter/Type/EntityFilterOptions.php
@@ -9,12 +9,12 @@ use PaLabs\DatagridBundle\DataSource\Filter\Options\BaseFilterOptions;
 class EntityFilterOptions extends BaseFilterOptions
 {
     public function entityForm(string $entityForm) {
-        $this->filterOptions['entity_form'] = $entityForm;
+        $this->formOptions['entity_form'] = $entityForm;
         return $this;
     }
 
     public function entityOptions(array $entityFormOptions) {
-        $this->filterOptions['entity_options'] = $entityFormOptions;
+        $this->formOptions['entity_options'] = $entityFormOptions;
         return $this;
     }
 }
