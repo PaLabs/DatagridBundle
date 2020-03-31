45dbe17b1dc164a574651a63036138c462cd0027
lewbor <boris.alperin@gmail.com>
2020-03-31 12:23:08 +0700

    Fixed OptFilterBuilder

--- src/DataSource/Filter/OptFilterBuilder.php
+++ src/DataSource/Filter/OptFilterBuilder.php
@@ -4,7 +4,6 @@
 namespace PaLabs\DatagridBundle\DataSource\Filter;
 
 
-
 use PaLabs\DatagridBundle\DataSource\Filter\Options\FilterOptions;
 
 class OptFilterBuilder
@@ -16,11 +15,14 @@ class OptFilterBuilder
         $this->builder = $builder;
     }
 
-    public function add(string $name, string $filterClass, ?FilterOptions $filterOptions = null): self {
-        $this->builder->add($name, $filterClass,
+    public function add(string $name, string $filterClass, ?FilterOptions $filterOptions = null): self
+    {
+        $this->builder->add(
+            $name,
+            $filterClass,
             $filterOptions === null ? [] : $filterOptions->formOptions(),
-            null,
-            $filterOptions === null ? [] : $filterOptions->filterOptions());
+            $filterOptions === null ? [] : $filterOptions->filterOptions()
+        );
         return $this;
     }
 }
