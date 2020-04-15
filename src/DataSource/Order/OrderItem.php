a28d0a17eff882fe37d599bb3517dc5004a25b43
lewbor <boris.alperin@gmail.com>
2020-04-15 12:35:32 +0700

    Updated translations, add order methods to OrderItem

--- src/DataSource/Order/OrderItem.php
+++ src/DataSource/Order/OrderItem.php
@@ -11,9 +11,20 @@ class OrderItem
     protected string $field;
     protected OrderDirection $direction;
 
-    public function __construct(string $field, ?OrderDirection $direction = null) {
+    public function __construct(string $field, ?OrderDirection $direction = null)
+    {
         $this->field = $field;
         $this->direction = $direction === null ? OrderDirection::$ASC : $direction;
+    }
+
+    public static function ASC(string $field): OrderItem
+    {
+        return new OrderItem($field, OrderDirection::$ASC);
+    }
+
+    public static function DESC(string $field): OrderItem
+    {
+        return new OrderItem($field, OrderDirection::$DESC);
     }
 
     public function getField(): string
