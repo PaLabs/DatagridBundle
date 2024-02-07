4349357ead1d5e16f780810e62f6b94d2279bf81
lewbor <boris.alperin@gmail.com>
2024-02-07 11:23:32 +0700

    Fix Date filter transformer

--- src/DataSource/Filter/Form/Date/DateFilterModelTransformer.php
+++ src/DataSource/Filter/Form/Date/DateFilterModelTransformer.php
@@ -28,8 +28,8 @@ class DateFilterModelTransformer implements DataTransformerInterface
         }
         return new DateFilterData(
             $value[DateFilterForm::PERIOD_FIELD],
-            $value[DateFilterForm::START_FIELD],
-            $value[DateFilterForm::END_FIELD]
+            $value[DateFilterForm::START_FIELD ?? null],
+            $value[DateFilterForm::END_FIELD ?? null]
         );
     }
 }
